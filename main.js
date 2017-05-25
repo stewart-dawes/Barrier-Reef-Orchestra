baab4ac43de3b7353a92316c7dc68ddaf921780d
stewart-dawes <s2_d@hotmail.com>
2017-05-25 19:24:16 +1000

    Upload of offline edits
    Made changes offline due to work commitments.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ showSlides();
 
 function showSlides() {
     var i;
-	var dots = document.getElementsByClassName("dot");
+    var dots = document.getElementsByClassName("dot");
     var slides = document.getElementsByClassName("mySlides");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
@@ -11,13 +11,13 @@ function showSlides() {
     slideIndex++;
     if (slideIndex> slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-	
-	for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" dot_active", "");
-  }
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
 
-	dots[slideIndex-1].className += " dot_active";
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" dot_active", "");
+    }
+
+    dots[slideIndex-1].className += " dot_active";
 }
 
 function plusSlides(n) {

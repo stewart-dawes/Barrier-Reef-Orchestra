3f87aa87a9b6cbc5ab9f6d8361407eed241f259c
stewart-dawes <s2_d@hotmail.com>
2017-05-26 20:02:36 +1000

    last upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ showSlides();
 
 function showSlides() {
     var i;
+    var dots = document.getElementsByClassName("dot");
     var slides = document.getElementsByClassName("mySlides");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
@@ -10,7 +11,13 @@ function showSlides() {
     slideIndex++;
     if (slideIndex> slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 3000); // Change image every 3 seconds
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" dot_active", "");
+    }
+
+    dots[slideIndex-1].className += " dot_active";
 }
 
 function plusSlides(n) {
@@ -21,7 +28,6 @@ function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
-
 $(document).ready(function() {
 
     var description = [
@@ -30,9 +36,9 @@ $(document).ready(function() {
         "http://placehold.it/100x100"
     ];
 
-    var size = description.length
+    var size = description.length;
     $('.item img').each(function() {
-        var x = Math.floor(size * Math.random())
+        var x = Math.floor(size * Math.random());
         if ($(this).hasClass("people")) {
             $(this).attr("src", description[x]);
         }

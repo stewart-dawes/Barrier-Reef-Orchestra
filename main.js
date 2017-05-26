44f2a6fde192cd6ca0ea68c1314088a516a64245
stewart-dawes <s2_d@hotmail.com>
2017-05-26 12:47:25 +1000

    Update offline work
    Updated offline work to most files including changes to menu bars and addition of pictures and other content.

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ showSlides();
 
 function showSlides() {
     var i;
-    var dots = document.getElementsByClassName("dot");
     var slides = document.getElementsByClassName("mySlides");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
@@ -11,13 +10,7 @@ function showSlides() {
     slideIndex++;
     if (slideIndex> slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 3000); // Change image every 2 seconds
-
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" dot_active", "");
-    }
-
-    dots[slideIndex-1].className += " dot_active";
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
 
 function plusSlides(n) {
@@ -29,3 +22,42 @@ function currentSlide(n) {
 }
 
 
+$(document).ready(function() {
+
+    var description = [
+        "http://placehold.it/300x300",
+        "http://placehold.it/200x200",
+        "http://placehold.it/100x100"
+    ];
+
+    var size = description.length
+    $('.item img').each(function() {
+        var x = Math.floor(size * Math.random())
+        if ($(this).hasClass("people")) {
+            $(this).attr("src", description[x]);
+        }
+    });
+
+});
+var slideIndex2 = 1;
+showSlides2(slideIndex);
+
+function plusSlides2(n) {
+    showSlides2(slideIndex += n);
+}
+
+function currentSlide2(n) {
+    showSlides2(slideIndex = n);
+}
+
+function showSlides2(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides2");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex-1].style.display = "block";
+}
